eb87ea1914ec9442a9c189e001ccf624df12cd2a
guettafa <irontitou5@gmail.com>
2025-01-26 19:28:44 -0500

    Deleted SDL2_Image from /lib folder

--- BCI-GUI/src/window/menu.cpp
+++ BCI-GUI/src/window/menu.cpp
@@ -2,11 +2,11 @@
 
 void Menu::ShowMenu()
 {
-	ImGui::Begin("SomeTest");
+	ImGui::Begin("");
 		ImGui::Text("Hi ImGui");
 	ImGui::End();
 
-	ImGui::Begin("MainWindow");
+	ImGui::Begin("Profile Selector");
 		ImGui::Text("FPS : %f", ImGui::GetIO().Framerate);
 	ImGui::End();
 }
